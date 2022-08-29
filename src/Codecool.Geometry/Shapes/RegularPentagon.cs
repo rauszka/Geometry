a1a16e25c02e46e8af52431b96fb89d57fb31f27
rauszka <retroperitonealis@gmail.com>
2022-08-29 17:32:26 +0200

    implement metods in Rectangle.cs

--- src/Codecool.Geometry/Shapes/RegularPentagon.cs
+++ src/Codecool.Geometry/Shapes/RegularPentagon.cs
@@ -7,20 +7,34 @@ namespace Codecool.Geometry.Shapes
     /// </summary>
     public class RegularPentagon : Shape
     {
+        private double _a;
+        private double _b;
+        private static string _areaFormula = "a×b";
+        private static string _perimeterFormula = "2×a+2×b";
+        public double _area;
+        public double _perimeter;
+
+        public Rectangle(double a, double b)
+        {
+            _a = a;
+            _b = b;
+            _area = a * b;
+            _perimeter = 2 * (a + b);
+        }
         /// <summary>
         ///     Gets formula for the area of the pentagon as a string.
         /// </summary>
-        public new static string AreaFormula => throw new NotImplementedException();
+        public new static string AreaFormula => _areaFormula;
 
         /// <summary>
         ///     Gets formula for the perimeter of the pentagon as a string.
         /// </summary>
-        public new static string PerimeterFormula => throw new NotImplementedException();
+        public new static string PerimeterFormula => _perimeterFormula;
 
         /// <inheritdoc/>
-        public override double Area => throw new NotImplementedException();
+        public override double Area => Area => _area;
 
         /// <inheritdoc/>
-        public override double Perimeter => throw new NotImplementedException();
+        public override double Perimeter => _perimeter;
     }
 }
