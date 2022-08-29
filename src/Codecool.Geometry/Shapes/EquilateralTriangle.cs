99e95e65cc61aa065f55e899b63fbd1b3f9b4dfd
rauszka <retroperitonealis@gmail.com>
2022-08-29 17:29:24 +0200

    implenet methods in EquilateralTriangle.cs

--- src/Codecool.Geometry/Shapes/EquilateralTriangle.cs
+++ src/Codecool.Geometry/Shapes/EquilateralTriangle.cs
@@ -7,14 +7,25 @@ namespace Codecool.Geometry.Shapes
     /// </summary>
     public class EquilateralTriangle : Triangle
     {
+        private double _a;
+        private static string _areaFormula = "a×a×sqrt(3)/4";
+        private static string _perimeterFormula = "3×a";
+
+        public EquilateralTriangle(double a) : base(a, a, a)
+        {
+            _a = a;
+            _area = Math.Pow(_a, 2) * Math.Sqrt(0.75);
+            _perimeter = 3 * _a;
+        }
+
         /// <summary>
         ///     Gets formula for the area of the triangle as a string.
         /// </summary>
-        public new static string AreaFormula => throw new NotImplementedException();
+        public new static string AreaFormula => _areaFormula;
 
         /// <summary>
         ///     Gets formula for the perimeter of the triangle as a string.
         /// </summary>
-        public new static string PerimeterFormula => throw new NotImplementedException();
+        public new static string PerimeterFormula => _perimeterFormula;
     }
 }
