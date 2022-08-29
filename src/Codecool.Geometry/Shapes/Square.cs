16982533042743c62f6889c1d441a3ad2ce1ada5
rauszka <retroperitonealis@gmail.com>
2022-08-29 19:10:12 +0200

    implemented methods in Square.cs

--- src/Codecool.Geometry/Shapes/Square.cs
+++ src/Codecool.Geometry/Shapes/Square.cs
@@ -7,14 +7,29 @@ namespace Codecool.Geometry.Shapes
     /// </summary>
     public class Square : Rectangle
     {
+        private double _a;
+        private static string _areaFormula = "a×a";
+        private static string _perimeterFormula = "4×a";
+        public Square(double side) : base(side, side)
+        {
+            _a = side;
+            _area = _a * _a;
+            _perimeter = 4 * _a;
+        }
+
         /// <summary>
         ///     Gets formula for the area of the rectangle as a string.
         /// </summary>
-        public new static string AreaFormula => throw new NotImplementedException();
+        public new static string AreaFormula => _areaFormula;
 
         /// <summary>
         ///     Gets formula for the perimeter of the rectangle as a string.
         /// </summary>
-        public new static string PerimeterFormula => throw new NotImplementedException();
+        public new static string PerimeterFormula =>_perimeterFormula;
+
+        public override string ToString()
+        {
+            return $"Square, a = {Math.Round(_a, 2)}";
+        }
     }
 }
